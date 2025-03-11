394985c039870fa15aba8935dfbeab51d6586588
trimble-mmattera <61026975+trimble-mmattera@users.noreply.github.com>
2025-03-11 15:18:16 -0400

    Add public-samples/testing-ai-code-generation-av84sq/infrastructure/terraform/modules/waf/outputs.tf

---
+++ infrastructure/terraform/modules/waf/outputs.tf
@@ -0,0 +1,14 @@
+output "web_acl_id" {
+  description = "ID of the WAF Web ACL"
+  value       = aws_wafv2_web_acl.main.id
+}
+
+output "web_acl_arn" {
+  description = "ARN of the WAF Web ACL"
+  value       = aws_wafv2_web_acl.main.arn
+}
+
+output "waf_logs_bucket" {
+  description = "Name of the S3 bucket storing WAF logs"
+  value       = aws_s3_bucket.waf_logs.bucket
+}
